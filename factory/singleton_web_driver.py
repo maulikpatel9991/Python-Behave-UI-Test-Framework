be323aca70eeba370bf0c3bda2dd24c872fc97a0
maulikpatel9991 <patelmaulik9991@gmail.com>
2025-03-30 17:21:56 -0400

    Latest Code

--- factory/singleton_web_driver.py
+++ factory/singleton_web_driver.py
@@ -25,25 +25,12 @@ class WebDriverManager:
             if browser == "chrome":
                 options = webdriver.ChromeOptions()
                 options.add_argument("--disable-gpu")
-                options.add_argument("--window-size=1920,1080")
-                user_data_dir = tempfile.mkdtemp()  # Ensures uniqueness
-                options.add_argument(f"user-data-dir={user_data_dir}")   
+                options.add_argument("--window-size=1920,1080")   
                 if mode == "headless":
                     options.add_argument("--headless")
                 service = ChromeService(ChromeDriverManager().install())
                 WebDriverManager._driver = webdriver.Chrome(service=service, options=options)
 
-                               # Cleanup the user data directory after the test run
-                def cleanup():
-                    try:
-                        if os.path.exists(user_data_dir):
-                            shutil.rmtree(user_data_dir)  # Delete the temp directory after use
-                    except Exception as e:
-                        print(f"Error cleaning up user data dir: {e}")
-
-                # Register cleanup to ensure user data dir is cleaned up
-                import atexit
-                atexit.register(cleanup)
 
             elif browser == "firefox":
                 options = FirefoxOptions()
@@ -68,29 +55,29 @@ class WebDriverManager:
             WebDriverManager._driver.quit()
             WebDriverManager._driver = None
 
-    # @staticmethod
-    # def parse_arguments():
-    #     parser = argparse.ArgumentParser(description="WebDriver Configuration")
+    @staticmethod
+    def parse_arguments():
+        parser = argparse.ArgumentParser(description="WebDriver Configuration")
 
-    #     parser.add_argument("--browser", choices=["chrome", "firefox", "edge"], default="chrome",
-    #                         help="Choose browser for test execution")
-    #     parser.add_argument("--mode", choices=["web", "headless"], default="web",
-    #                         help="Choose browser execution mode (default = web)")
+        parser.add_argument("--browser", choices=["chrome", "firefox", "edge"], default="chrome",
+                            help="Choose browser for test execution")
+        parser.add_argument("--mode", choices=["web", "headless"], default="web",
+                            help="Choose browser execution mode (default = web)")
         
-    #     parser.add_argument("--environment", choices=["staging", "dev", "production"], default="staging",
-    #                     help="==> Environment to execute the tests (default = staging). Find the app URLs in properties file")
+        parser.add_argument("--environment", choices=["staging", "dev", "production"], default="staging",
+                        help="==> Environment to execute the tests (default = staging). Find the app URLs in properties file")
         
-    #     parser.add_argument("--orientation", choices=["Landscape", "Portrait"], default="Portrait",
-    #                     help="==> Screen orientation for mobile executions (default = Portrait)")
+        parser.add_argument("--orientation", choices=["Landscape", "Portrait"], default="Portrait",
+                        help="==> Screen orientation for mobile executions (default = Portrait)")
     
-    #     parser.add_argument("--resolution", choices=["1024x768", "1280x1024", "1600x1200", "1920x1080"], default="1280x1024",
-    #                         help="==> Resolution for web execution (default = 1280x1024)")
+        parser.add_argument("--resolution", choices=["1024x768", "1280x1024", "1600x1200", "1920x1080"], default="1280x1024",
+                            help="==> Resolution for web execution (default = 1280x1024)")
         
-    #     parser.add_argument("--tags", default="",
-    #                         help="==> Feature(s) / Scenario(s) to be executed (separate tags by comma)")
+        parser.add_argument("--tags", default="",
+                            help="==> Feature(s) / Scenario(s) to be executed (separate tags by comma)")
         
-    #     parser.add_argument("--exclude", choices=["wip", "skip", "bug", "slow", "obsolete", ""], default="",
-    #                     help="==> Scenarios to be ignored (e.g., wip)")
+        parser.add_argument("--exclude", choices=["wip", "skip", "bug", "slow", "obsolete", ""], default="",
+                        help="==> Scenarios to be ignored (e.g., wip)")
 
 
-    #     return parser.parse_args()
+        return parser.parse_args()
